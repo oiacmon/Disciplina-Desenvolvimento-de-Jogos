dfd29c33be04e0909e190836935233f26d629a85
Oiacmon <caiodornellas@outlook.com>
2026-09-15 20:13:02 -0300

    Aula 15-09

---
+++ DesenvolvimentodeJogos/Aula 15-09/sprintesheet.js
@@ -0,0 +1,46 @@
+function Sprintesheet(ctx, imagem, linhas, colunas){
+    this.ctx = ctx;
+    this.imagem = imagem;
+    this.numLinha = linhas;
+    this.numColunas = colunas;
+    this.intervalo = 0;
+    this.linha = 0;
+    this.coluna = 0;
+    
+}
+
+
+Sprintesheet.prototype = {
+    proximoQuadro: function(){
+        var agora = new Date().getTime();
+        if(!this.ultimoTempo) this.ultimoTempo = agora;
+        
+        if(agora - this.ultimoTempo < this.intervalo) return;
+
+        if(this.coluna < this.numColunas -1)
+            this.coluna++;
+        else
+            this.coluna = 0;
+
+        this.ultimoTempo = agora;
+    },
+
+    desenhar: function(x,y){
+        var largura = this.imagem.width / this.numColunas;
+        var altura = this.imagem.height / this.numLinha;
+        
+        this.context.drawImage(
+            this.imagem,
+            largura * this.coluna,
+            altura * this.linha,
+            largura,
+            altura,
+            x,
+            y,
+            largura,
+            altura
+
+        );
+    }
+
+}
