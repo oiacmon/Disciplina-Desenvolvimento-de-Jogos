80ff1c71abc375de20d23e2a2e492139d714abd2
Aluno UniAcademia <alunouni@labcesac.net>
2026-09-16 19:56:31 -0300

    Aula 16-09

---
+++ DesenvolvimentodeJogos/Aula 16-09/ATIVIDADE - 2/homem.js
@@ -0,0 +1,90 @@
+var Homem_DIREITA = 1;
+var Homem_ESQUERDA = 2;
+var Homem_CIMA = 3;
+var Homem_BAIXO = 4;
+
+function Homem(context, teclado, imagem) { 
+   this.context = context; 
+   this.teclado = teclado; 
+   this.x = 0; 
+   this.y = 0; 
+   this.velocidade = 10;
+
+   // Criando a spritesheet a partir da imagem recebida
+   this.sheet = new Spritesheet(context, imagem, 4, 6);
+   this.sheet.intervalo = 60;
+
+   // Estado inicial
+   this.andando = false;
+   this.direcao = Homem_DIREITA;
+} 
+Homem.prototype = { 
+   atualizar: function() { 
+      if (this.teclado.pressionada(SETA_DIREITA)) {
+         // Se já não estava neste estado...
+         if (! this.andando || this.direcao != Homem_DIREITA) {
+            // Seleciono o quadro da spritesheet
+            this.sheet.linha = 2;
+            this.sheet.coluna = 0;
+         }
+
+         // Configuro o estado atual
+         this.andando = true;
+         this.direcao = Homem_DIREITA;
+
+         // Neste estado, a animação da spritesheet deve rodar
+         this.sheet.proximoQuadro();
+
+         // Desloco o Homem
+         this.x += this.velocidade;
+      }
+
+      else if (this.teclado.pressionada(SETA_ESQUERDA)) {
+         if (! this.andando || this.direcao != Homem_ESQUERDA) {
+            this.sheet.linha = 1; 
+            this.sheet.coluna = 0;
+         }
+
+         this.andando = true;
+         this.direcao = Homem_ESQUERDA;
+         this.sheet.proximoQuadro();
+         this.x -= this.velocidade;  // E aqui é sinal de menos!
+      }
+
+      else if (this.teclado.pressionada(SETA_CIMA)) {
+         if (! this.andando || this.direcao != Homem_CIMA) {
+            this.sheet.linha = 3; 
+            this.sheet.coluna = 0;
+         }
+         this.andando = true;
+         this.direcao = Homem_CIMA;
+         this.sheet.proximoQuadro();
+         this.y -= this.velocidade; 
+      }
+      
+      else if (this.teclado.pressionada(SETA_BAIXO)) {
+         if (! this.andando || this.direcao != Homem_BAIXO) {
+            this.sheet.linha = 0; 
+            this.sheet.coluna = 0;
+         }
+         this.andando = true;
+         this.direcao = Homem_BAIXO;
+         this.sheet.proximoQuadro();
+         this.y += this.velocidade;
+      }
+
+      else {
+         if (this.direcao == Homem_DIREITA) 
+            this.sheet.coluna = 0; 
+         else if (this.direcao == Homem_ESQUERDA) 
+            this.sheet.coluna = 1; 
+
+         this.sheet.linha = 0; 
+         // Não chamo proximoQuadro!
+         this.andando = false; 
+      }
+   }, 
+   desenhar: function() { 
+      this.sheet.desenhar(this.x, this.y);         
+   } 
+}
