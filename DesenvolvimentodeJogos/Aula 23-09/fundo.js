e77cd9d702b45cc8373e684df650e2a8175685ad
Oiacmon <caiodornellas@outlook.com>
2026-09-24 10:35:32 -0300

    Feat: Finalizando Aula do dia 23-09

--- DesenvolvimentodeJogos/Aula 23-09/fundo.js
+++ DesenvolvimentodeJogos/Aula 23-09/fundo.js
@@ -1,44 +1,42 @@
+var VERTICAL = 1;
+var HORIZONTAL = 2;
+
 function Fundo(context, imagem, direcao) {
    this.context = context;
    this.imagem = imagem;
    this.velocidade = 0;
    this.posicaoEmenda = 0;
-   this.direcao = vertical;
+   this.direcao = direcao;
 }
+
 Fundo.prototype = {
    atualizar: function() {
-      // Atualizar a posição de emenda
       this.posicaoEmenda += this.velocidade;
       
-      if(this.direcao == vertical && this.posicaoEmenda > this.imagem.height){
+      if (this.direcao == VERTICAL && this.posicaoEmenda > this.imagem.height) {
          this.posicaoEmenda = 0;
       }
-      else if(this.direcao == horizontal && this.posicaoEmenda > this.imagem.width){
+      else if (this.direcao == HORIZONTAL && this.posicaoEmenda > this.imagem.width) {
          this.posicaoEmenda = 0;
       }
-      
    },
+
    desenhar: function() {
       var img = this.imagem;
-        // Para facilitar a escrita :D
-      if (this.direcao == vertical){         
-         // Primeira cópia
-         var posicaoY = this.posicaoEmenda - img.height;
-         this.context.drawImage(img, 0, posicaoY, img.width, img.height);
-         
-         // Segunda cópia
-         posicaoY = this.posicaoEmenda;
-         this.context.drawImage(img, 0, posicaoY, img.width, img.height);
-      }   
-      else if (this.direcao == horizontal){
-         
-         var posicaoX = this.posicaoEmenda - img.width;
-         this.context.drawImage(img, 0, posicaoX, img.width, img.height);
-         
-         // Segunda cópia
-         posicaoX = this.posicaoEmenda;
-         this.context.drawImage(img, 0, posicaoX, img.width, img.height);
-      }   
-      }  
-   }
 
+      if (this.direcao == VERTICAL) {
+         var posY = this.posicaoEmenda - img.height;
+         this.context.drawImage(img, 0, posY);
+         
+         posY = this.posicaoEmenda;
+         this.context.drawImage(img, 0, posY);
+      }
+      else if (this.direcao == HORIZONTAL) {
+         var posX = this.posicaoEmenda - img.width;
+         this.context.drawImage(img, posX, 0);
+         
+         posX = this.posicaoEmenda;
+         this.context.drawImage(img, posX, 0);
+      }
+   }
+};
